cca622be328cd8f7ffddeb8ea7306c0fa1827a9c
ddolan88 <104475059+ddolan88@users.noreply.github.com>
2023-04-03 13:16:40 -0700

    lab 2

---
+++ LabTwo/AreaCircumferance.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+package LabTwo;
+
+public class AreaCircumferance {
+    // Area = pi * r * r
+    // Circumference = r * r
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the radius: ");
+        double = radius = scanner.nextDouble();
+
+        System.out.println("Area of the circle is: " + Math.PI * Math.pow(radius, 2));
+        System.out.println("Circumference of the circle is: " + Math.pow(radius, 2));
+    };
+}
